0a9eebc43ef6a2d121535a76019c667f61175516
HMinng <jackhm18@gmail.com>
2017-06-05 22:15:32 +0800

    修改bug

--- src/AtsLog.php
+++ src/AtsLog.php
@@ -3,10 +3,10 @@ use HMinng\Log\Base\Base;
 
 class AtsLog extends Base
 {
-    public static $input = array();
-
-    public static $traceID = NULL;
-
+    public static $input   = array();
+    
+    public static $traceID = null;
+    
     /**
      * @param string $message 错误信息
      * @param array $params 包括input，info信息
@@ -15,7 +15,7 @@ class AtsLog extends Base
     {
         self::process('error', $message, $params, $force);
     }
-
+    
     /**
      * @param string $message 警告信息
      * @param array $params 包括input，info信息
@@ -24,17 +24,17 @@ class AtsLog extends Base
     {
         self::process('waring', $message, $params, $force);
     }
-
+    
     /**
      * @param string $message 打印信息
-     * @param int  $traceId   链路id
+     * @param int $traceId 链路id
      * @param array $params 包括input，info信息
      */
     public static function info($message, $params = array(), $force = false)
     {
         self::process('info', $message, $params, $force);
     }
-
+    
     /**
      * @param string $message debug信息
      * @param array $params 包括input，info信息
@@ -43,43 +43,49 @@ class AtsLog extends Base
     {
         self::process('debug', $message, $params, $force);
     }
-
+    
     private static function genTraceId()
     {
         $time = microtime(true);
+        
+        if (strpos($time, '.') === false) {
+            $time = $time.'.'.rand(0, 9999);
+        }
+        
         $time = explode('.', $time);
-
-        $rand = rand(0,999);
-
-        $time = $time[0] . $time[1] * 1000 . $rand;
-        return $time;
+        
+        $rand = rand(0, 999);
+        
+        $time = $time[0].$time[1] * 1000 .$rand;
+        
+        return uniqid($time, true);
     }
-
+    
     private static function setParams($params, $force)
     {
         if ( ! empty(self::$input)) {
             self::$params['input'] = json_encode(self::$input);
         }
-
+        
         if (array_key_exists('info', $params) && ! empty($params['info'])) {
             self::$params['info'] = json_encode($params['info']);
         }
-
+        
         if (is_null(self::$traceID) || $force) {
             self::$traceID = self::genTraceId();
         }
-
+        
         self::$params['id'] = self::$traceID;
-
+        
         return true;
     }
-
+    
     private static function process($level, $message, $params, $force)
     {
         self::$level = $level;
-
+        
         self::setParams($params, $force);
-
+        
         self::write($message);
     }
 }
